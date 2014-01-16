52d11a0d8385a66ea7e96a7f88a6dda0bf2146de
Andy Miller <rhuk@mac.com>
2014-01-16 15:13:21 -0700

    better integration for statslink_allowed setting

--- components/com_kunena/site/controller/page/statistics/display.php
+++ components/com_kunena/site/controller/page/statistics/display.php
@@ -40,15 +40,18 @@ class ComponentKunenaControllerPageStatisticsDisplay extends KunenaControllerDis
 
 		if (!$this->config->get('showstats'))
 		{
-			return false;
+			throw new KunenaExceptionAuthorise(JText::_('COM_KUNENA_NO_ACCESS'), '404');
 		}
 
 		$statistics = KunenaForumStatistics::getInstance();
 		$statistics->loadGeneral();
 		$this->setProperties($statistics);
 
+		$me = KunenaUserHelper::getMyself();
+		$moderator = intval($me->isModerator()) + intval($me->isAdmin());
+
 		$this->latestMemberLink = KunenaFactory::getUser(intval($this->lastUserId))->getLink();
-		$this->statisticsUrl = KunenaRoute::_('index.php?option=com_kunena&view=statistics');
+		$this->statisticsUrl = KunenaFactory::getProfile()->getStatisticsURL();
 		$this->userlistUrl = KunenaFactory::getProfile()->getUserListUrl();
 
 		return true;
