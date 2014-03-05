404a62cd75469cb7beb9cf0b8704c1fb07d17965
Matias Griese <matias@kunena.org>
2014-03-05 14:00:36 +0200

    Add moderator statistics view to admin

---
+++ components/com_kunena/admin/controllers/statistics.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Kunena Component
+ * @package Kunena.Administrator
+ * @subpackage Controllers
+ *
+ * @copyright (C) 2008 - 2013 Kunena Team. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL
+ * @link http://www.kunena.org
+ **/
+defined ( '_JEXEC' ) or die ();
+
+/**
+ * Kunena Backend Statistics Controller
+ *
+ * @since 3.1
+ */
+class KunenaAdminControllerStatistics extends KunenaController {
+}
